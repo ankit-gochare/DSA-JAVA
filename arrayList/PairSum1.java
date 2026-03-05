a079a92d349400edda593a95f03eeaa9770c69d7
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-05 18:57:57 +0530

    Pair sum in sorted array

--- arrayList/PairSum1.java
+++ arrayList/PairSum1.java
@@ -16,7 +16,7 @@ public class PairSum1{
     public static boolean pairSum1(ArrayList<Integer> list , int target){
         
         for(int i =0 ; i<list.size();i++){
-            for(int j = i+1; j<list.size()-1;j++){
+            for(int j = i+1; j<list.size();j++){
                 if(list.get(i) + list.get(j) == target){
                     return true;
                 }
