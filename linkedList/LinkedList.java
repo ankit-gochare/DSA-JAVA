c8afbf1a661d9376d9eae83c22795f529a27b1d8
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-07 01:35:34 +0530

    Linked List methods added

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -138,6 +138,180 @@ public class LinkedList{
         System.out.println("null");
     }
     // time complexity O(n)
+
+
+    // 1. remove from first place
+    public int removeFirst(){
+        // empty list or size ==1
+        if(size==0){
+            System.out.println("Linked List is empty");
+            return Integer.MIN_VALUE;
+        }
+        else if(size == 1){
+            int val = head.data;
+            head = tail = null ;
+            size =0;
+            return val;
+        }
+
+        // otherwise 
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+    // time complexity O(1)
+
+    // 2. remove last
+    public int removeLast(){
+        // empty ll or size =1
+        if(size ==0){
+            System.out.println("LinkedList is empty");
+            return Integer.MIN_VALUE;
+        }
+        else if(size == 0){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        // other wise 
+        // find previous of tail
+        Node prev = head;
+        for(int i =0 ; i<size-2; i++){
+            prev = prev.next;
+        }
+
+        int val = prev.next.data; // tail data
+        prev.next = null;
+        tail = prev;
+        size--;
+        return val;
+    }
+    // time complexity is O(n)
+
+    // search (iterative)
+    // search for a key in a linked list
+    // return the position where it is found
+    // if not found , return -1
+
+    public int iterativeSearch(int key){
+        Node temp = head;
+
+        int i =0;
+        while(temp!=null){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1; // not found
+    }
+    // time complexity O(n)
+
+
+    // Search recursive
+    // search for a key in linked list
+    // return the position where it is found
+    // if not found -1 
+    // use recursion
+
+    // first find for head.next 
+    // for head it will be calculated automatically
+
+    // base case
+    // head.data == key return 0
+    
+    // amd after the call index = index +1
+
+    public int recursiveSearch(int key){
+        return helper(head , key);
+    }
+
+    public int helper(Node head , int key){
+        // empty list // base case
+        if(head == null){
+            return -1;
+        }
+
+        // found basecase
+        if(head.data == key){
+            return 0;
+        }
+
+        int index = helper(head.next , key);
+        if(index == -1){
+            return -1;
+        }
+        return index+1;
+    }
+    // time complexity = O(N)
+    // space complexity = O(n)
+
+
+    // reverse a linked list
+
+    // iterative approach
+    // 3 variable & 4 steps
+    
+    public void reverseLL(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr ;
+            curr = next;
+        }
+        // update the head
+        head = prev ;
+    }
+    // time complexity O(n)
+
+    // Find and remove nth node from the end
+    // iterative approach
+    // prev.next = next
+
+    // main task is finding the nth node from the end
+    // the nth node from the end is 
+    // size - n +1 th node from the start
+    // to remove nth 
+    // we have find prev of it
+    public void deleteNthFromEnd(int n ){
+        // calculate size 
+        int sz = 0;
+        Node temp = head ;
+
+        while(temp != null){
+            temp = temp.next;
+            sz++;
+        }
+
+        if(n==size){
+            head = head.next; // removefirst
+            size--;
+            return;
+        }
+
+        // sz-n
+        int i=1;
+        int iToFind = sz-n;
+        Node prev = head;
+        while(i<iToFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        size--;
+        return;
+    }
+    // time complexity = O(n)
+
+
     public static void main(String[] args){
         LinkedList ll = new LinkedList();
 
@@ -167,6 +341,39 @@ public class LinkedList{
         ll.add(3,23);
         ll.printList();
         System.out.println(size);
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        ll.removeFirst();
+        ll.printList();
+
+        ll.removeLast();
+        ll.printList();
+
+        System.out.println(ll.iterativeSearch(23));
+        System.out.println(ll.iterativeSearch(35));
+        System.out.println(ll.recursiveSearch(23));
+        System.out.println(ll.recursiveSearch(35));
+
+        ll.reverseLL();
+        ll.printList();
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(9);
+        ll.addFirst(99);
+        ll.addFirst(20);
+        ll.addFirst(30);
+
+        ll.printList();
         
+        ll.deleteNthFromEnd(2);
+        ll.printList();
+        ll.deleteNthFromEnd(3);
+
+        ll.printList();
     }
 }
