6b6bf3e19be35aeb8f3614e9c17868317ea83a38
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-09 23:02:42 +0530

    stack using the java collections

---
+++ stackLIFO/StackC.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+public class StackC{
+
+    public static void main(String[] args){
+        Stack<Character> sc = new Stack<>();
+
+        sc.push('a');
+        sc.push('b');
+        sc.push('c');
+
+        System.out.println(sc);
+    }
+}
