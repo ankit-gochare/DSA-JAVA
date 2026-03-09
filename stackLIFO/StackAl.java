fbe00ede1b2c9e4026e4404faa70f00cc1618abc
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-09 22:52:47 +0530

    stack uisng ArrayList

---
+++ stackLIFO/StackAl.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class StackAl{
+
+    // Stack implmentation usong ArrayList
+    static class stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        // check if the stack is empty
+        public static boolean isEmpty(){
+            return list.size()==0;
+        }
+
+        // push 
+        public static void push(int data){
+            list.add(data);
+        }
+
+        // pop
+        public static int pop(){
+            // if empty return -1
+            if(isEmpty()){
+                return -1;
+            }
+
+            int top = list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        // peek
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+
+            return list.get(list.size()-1);
+        }
+    }
+    public static void main(String[] args){
+        stack s = new stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+
+        // print and delete the elements
+        while(!s.isEmpty()){
+            System.out.print(s.peek() + " ");
+            s.pop();
+        }
+        System.out.println();
+    }
+}
