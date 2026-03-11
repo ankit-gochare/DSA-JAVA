bd4a1b47e57536933404fff813a87d1926ed1ef9
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-11 23:08:46 +0530

    Queue implementation using Linked list

---
+++ queue/QueueLL.java
@@ -0,0 +1,84 @@
+public class QueueLL{
+
+    // Queue implementation usind Linked list
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Queue{
+        static Node head = null;
+        static Node tail = null;
+
+        // is empty
+        public static boolean isEmpty(){
+            return head == null && tail == null;
+        }
+
+        // add 
+        public static void add(int data ){
+            // create a new Node
+            Node newNode = new Node(data);
+
+            // if emprty
+            if(isEmpty()){
+                head = tail = newNode;
+                return;
+            }
+
+            tail.next = newNode;
+            tail = newNode;
+        }
+
+        // remove
+        public static int remove(){
+            // if empty
+            if(isEmpty()){
+                System.out.println("The Queue is empty");
+                return -1;
+            }
+
+            int front = head.data;
+
+            // if last element
+            if(tail == head){
+                tail = head = null;
+            }
+            else{
+                head = head.next;
+            }
+
+            return front;
+        }
+
+        // peek()
+        public static int peek(){
+            // if empty
+            if(isEmpty()){
+                System.out.println("The Queue is empty");
+                return -1;
+            }
+
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args){
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        while(!q.isEmpty()){
+            System.out.print(q.peek() + " ");
+            q.remove();
+        }
+    }
+}
