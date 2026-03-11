f3b599274774703c682a006de1846264e3fe5013
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-11 23:13:06 +0530

    Queue uising Java collection framework

---
+++ queue/QueueJCF.java
@@ -0,0 +1,32 @@
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueJCF{
+    public static void main(String[] args){
+        Queue<Integer> q1 = new LinkedList<>();
+        q1.add(1);
+        q1.add(2);
+        q1.add(3);
+        q1.add(4);
+
+        System.out.println(q1);
+
+        q1.remove();
+        q1.remove();
+        System.out.println(q1);
+
+        Queue<Integer> q2 = new ArrayDeque<>();
+        q2.add(1);
+        q2.add(2);
+        q2.add(3);
+        q2.add(4);
+
+        System.out.println(q2);
+
+        q2.remove();
+        q2.remove();
+        System.out.println(q2);
+
+    }
+}
