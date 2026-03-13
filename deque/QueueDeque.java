05504c7ff442b92f10678c4eb19c9ded9330f1e5
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-13 16:24:15 +0530

    Queue using Deque

---
+++ deque/QueueDeque.java
@@ -0,0 +1,38 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class QueueDeque{
+    
+    // Queue using Deque
+    static class Queue{
+        static Deque<Integer> dq = new LinkedList<>();
+
+        public void add(int data){
+            dq.addLast(data);
+        }
+
+        public int remove(){
+            return dq.removeFirst();
+        }
+
+        public int peek(){
+            return dq.getFirst();
+        }
+    }  
+    public static void main(String[] args){
+        Queue q = new Queue();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        System.out.println(q.remove());
+        // System.out.println(q.remove());
+    }
+}
