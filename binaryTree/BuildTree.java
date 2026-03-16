59f2e70c4dde28471466f0091863ad1174123471
Ankit Gochare <ankitgochare35@gmail.com>
2026-03-16 16:13:58 +0530

    Build Binary tree preorder

---
+++ binaryTree/BuildTree.java
@@ -0,0 +1,45 @@
+public class BuildTree{
+
+    // Build tree preorder
+    // root left right
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data ){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree{
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]){
+            idx++;
+
+            // empty node // not existing 
+            if(nodes[idx] == -1){
+                return null;
+            }
+
+            // create a node 
+            Node newNode = new Node(nodes[idx]);
+
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode; 
+        }
+    }
+    
+    public static void main(String[] args){
+        int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+        BinaryTree bt = new BinaryTree();
+        Node root = bt.buildTree(nodes);
+        System.out.println(root.data);
+    }
+}
